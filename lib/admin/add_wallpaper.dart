8b60cd2221a8773bda59df5921a02f8337008217
prathamesh-mali <prathameshmali850@gmail.com>
2024-03-19 01:01:54 +0530

    Firebase Login

--- lib/admin/add_wallpaper.dart
+++ lib/admin/add_wallpaper.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AddWallpaper extends StatefulWidget {
   const AddWallpaper({super.key});
@@ -8,17 +10,117 @@ class AddWallpaper extends StatefulWidget {
 }
 
 class _AddWallpaperState extends State<AddWallpaper> {
+  List<String> categoriesitems = [
+    'Nature',
+    'Cars',
+    'Wildlife',
+    'Patterns',
+    'Foods',
+  ];
+
+  String? value;
+
+  ImagePicker _picker = ImagePicker();
+  File? selectedFile;
+
+  Future getImage() async {
+    var image = await _picker.pickImage(source: ImageSource.gallery);
+
+    selectedFile = File(image!.path);
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: Platform.isAndroid
+            ? const Icon(Icons.arrow_back_outlined)
+            : const Icon(Icons.arrow_back_ios_new),
+        title: const Text("Add Wallpapers"),
+        centerTitle: true,
+      ),
       body: Column(
         children: [
+          const SizedBox(
+            height: 40,
+          ),
+          Center(
+            child: Material(
+              elevation: 4,
+              borderRadius: BorderRadius.circular(12),
+              child: Container(
+                width: 300,
+                height: 350,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 1,
+                  ),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: const Icon(Icons.camera_alt_outlined),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
           Container(
-            child: const Text(
-              "Add Wallpapers",
-              style: TextStyle(
+            margin: const EdgeInsets.only(left: 10, right: 10),
+            width: MediaQuery.of(context).size.width,
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            decoration: BoxDecoration(
+              color: const Color(0xFFE0E0E0),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: DropdownButtonHideUnderline(
+              child: DropdownButton<String>(
+                items: categoriesitems
+                    .map(
+                      (items) => DropdownMenuItem<String>(
+                        value: items,
+                        child: Text(
+                          items,
+                          style: const TextStyle(
+                            fontSize: 18,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    )
+                    .toList(),
+                onChanged: (value) {
+                  setState(() {
+                    this.value = value;
+                  });
+                },
+                value: value,
+                hint: const Text("Select category"),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Container(
+              padding: const EdgeInsets.only(
+                  top: 15, bottom: 15, left: 60, right: 60),
+              margin: const EdgeInsets.only(
+                top: 20,
+              ),
+              decoration: BoxDecoration(
                 color: Colors.black,
-                fontSize: 26,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: const Text(
+                "Add",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
               ),
             ),
           )
